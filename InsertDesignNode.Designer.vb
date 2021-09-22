d5fa0d72a1914f0e277d7d946b922fe9f56ad188
nguyenvietvuongbk <88424822+nguyenvietvuongbk@users.noreply.github.com>
2021-09-22 20:12:19 +0700

    Update InsertDesignNode.Designer.vb

--- InsertDesignNode.Designer.vb
+++ InsertDesignNode.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class InsertDesignNode
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class designnodeaddlink
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,267 +20,165 @@ Partial Class InsertDesignNode
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(InsertDesignNode))
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.txtworktitle = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.txtworkdescription = New System.Windows.Forms.TextBox()
-        Me.ckfinish = New System.Windows.Forms.CheckBox()
-        Me.bttnOK = New System.Windows.Forms.Button()
-        Me.bttnCANCEL = New System.Windows.Forms.Button()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.bttn_addlink = New System.Windows.Forms.Button()
-        Me.bttnremovelink = New System.Windows.Forms.Button()
-        Me.bttnmodifylink = New System.Windows.Forms.Button()
-        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.txtresourcename = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtresourcelink = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.Button3 = New System.Windows.Forms.Button()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
-        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
-        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
-        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
-        Me.StatusStrip1.SuspendLayout()
-        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SplitContainer1.Panel1.SuspendLayout()
-        Me.SplitContainer1.Panel2.SuspendLayout()
-        Me.SplitContainer1.SuspendLayout()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.opt_folder = New System.Windows.Forms.RadioButton()
+        Me.opt_file = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(33, 32)
+        Me.Label1.Location = New System.Drawing.Point(2, 9)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(59, 13)
+        Me.Label1.Size = New System.Drawing.Size(80, 15)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Design title"
+        Me.Label1.Text = "reource name"
         '
-        'txtworktitle
+        'txtresourcename
         '
-        Me.txtworktitle.Location = New System.Drawing.Point(136, 32)
-        Me.txtworktitle.Name = "txtworktitle"
-        Me.txtworktitle.Size = New System.Drawing.Size(394, 20)
-        Me.txtworktitle.TabIndex = 1
+        Me.txtresourcename.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtresourcename.Location = New System.Drawing.Point(90, 6)
+        Me.txtresourcename.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.txtresourcename.Name = "txtresourcename"
+        Me.txtresourcename.Size = New System.Drawing.Size(506, 23)
+        Me.txtresourcename.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(522, 37)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(74, 25)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Browser"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtresourcelink
+        '
+        Me.txtresourcelink.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtresourcelink.Location = New System.Drawing.Point(90, 37)
+        Me.txtresourcelink.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.txtresourcelink.Name = "txtresourcelink"
+        Me.txtresourcelink.Size = New System.Drawing.Size(424, 23)
+        Me.txtresourcelink.TabIndex = 3
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(33, 75)
+        Me.Label2.Location = New System.Drawing.Point(2, 42)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(96, 13)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Design Description"
+        Me.Label2.Size = New System.Drawing.Size(69, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "reource link"
         '
-        'txtworkdescription
+        'GroupBox1
         '
-        Me.txtworkdescription.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.txtworkdescription.Location = New System.Drawing.Point(0, 0)
-        Me.txtworkdescription.Multiline = True
-        Me.txtworkdescription.Name = "txtworkdescription"
-        Me.txtworkdescription.ScrollBars = System.Windows.Forms.ScrollBars.Both
-        Me.txtworkdescription.Size = New System.Drawing.Size(395, 130)
-        Me.txtworkdescription.TabIndex = 3
-        '
-        'ckfinish
-        '
-        Me.ckfinish.AutoSize = True
-        Me.ckfinish.Location = New System.Drawing.Point(67, 137)
-        Me.ckfinish.Name = "ckfinish"
-        Me.ckfinish.Size = New System.Drawing.Size(53, 17)
-        Me.ckfinish.TabIndex = 4
-        Me.ckfinish.Text = "Finish"
-        Me.ckfinish.UseVisualStyleBackColor = True
-        '
-        'bttnOK
-        '
-        Me.bttnOK.Location = New System.Drawing.Point(282, 360)
-        Me.bttnOK.Name = "bttnOK"
-        Me.bttnOK.Size = New System.Drawing.Size(115, 28)
-        Me.bttnOK.TabIndex = 5
-        Me.bttnOK.Text = "OK"
-        Me.bttnOK.UseVisualStyleBackColor = True
-        '
-        'bttnCANCEL
-        '
-        Me.bttnCANCEL.Location = New System.Drawing.Point(414, 360)
-        Me.bttnCANCEL.Name = "bttnCANCEL"
-        Me.bttnCANCEL.Size = New System.Drawing.Size(115, 28)
-        Me.bttnCANCEL.TabIndex = 6
-        Me.bttnCANCEL.Text = "CANCEL"
-        Me.bttnCANCEL.UseVisualStyleBackColor = True
-        '
-        'Panel1
-        '
-        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Panel1.Location = New System.Drawing.Point(0, 0)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(395, 110)
-        Me.Panel1.TabIndex = 7
-        '
-        'bttn_addlink
-        '
-        Me.bttn_addlink.Location = New System.Drawing.Point(231, 308)
-        Me.bttn_addlink.Name = "bttn_addlink"
-        Me.bttn_addlink.Size = New System.Drawing.Size(80, 20)
-        Me.bttn_addlink.TabIndex = 8
-        Me.bttn_addlink.Text = "add link"
-        Me.bttn_addlink.UseVisualStyleBackColor = True
-        '
-        'bttnremovelink
-        '
-        Me.bttnremovelink.Location = New System.Drawing.Point(317, 308)
-        Me.bttnremovelink.Name = "bttnremovelink"
-        Me.bttnremovelink.Size = New System.Drawing.Size(80, 20)
-        Me.bttnremovelink.TabIndex = 9
-        Me.bttnremovelink.Text = "remove link"
-        Me.bttnremovelink.UseVisualStyleBackColor = True
-        '
-        'bttnmodifylink
-        '
-        Me.bttnmodifylink.Location = New System.Drawing.Point(403, 308)
-        Me.bttnmodifylink.Name = "bttnmodifylink"
-        Me.bttnmodifylink.Size = New System.Drawing.Size(80, 20)
-        Me.bttnmodifylink.TabIndex = 10
-        Me.bttnmodifylink.Text = "modify link"
-        Me.bttnmodifylink.UseVisualStyleBackColor = True
-        '
-        'ImageList1
-        '
-        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
-        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
-        Me.ImageList1.Images.SetKeyName(0, "workrecipe.png")
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(146, 334)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(126, 20)
-        Me.Button1.TabIndex = 11
-        Me.Button1.Text = "Select Design Folder"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(281, 334)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(100, 20)
-        Me.Button2.TabIndex = 12
-        Me.Button2.Text = "Open Folder"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.opt_folder)
+        Me.GroupBox1.Controls.Add(Me.opt_file)
+        Me.GroupBox1.Location = New System.Drawing.Point(2, 68)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.GroupBox1.Size = New System.Drawing.Size(594, 58)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "browser option"
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(387, 334)
+        Me.Button3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button3.Location = New System.Drawing.Point(384, 18)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(136, 20)
-        Me.Button3.TabIndex = 13
-        Me.Button3.Text = "Create Design Document"
+        Me.Button3.Size = New System.Drawing.Size(96, 25)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "CANCEL"
         Me.Button3.UseVisualStyleBackColor = True
         '
-        'Label3
+        'Button2
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(29, 177)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(100, 13)
-        Me.Label3.TabIndex = 14
-        Me.Label3.Text = "Adding resourcelink"
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(488, 16)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 25)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "OK"
+        Me.Button2.UseVisualStyleBackColor = True
         '
-        'StatusStrip1
+        'opt_folder
         '
-        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripStatusLabel2})
-        Me.StatusStrip1.Location = New System.Drawing.Point(0, 403)
-        Me.StatusStrip1.Name = "StatusStrip1"
-        Me.StatusStrip1.Size = New System.Drawing.Size(577, 22)
-        Me.StatusStrip1.TabIndex = 15
-        Me.StatusStrip1.Text = "StatusStrip1"
+        Me.opt_folder.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.opt_folder.AutoSize = True
+        Me.opt_folder.Location = New System.Drawing.Point(286, 21)
+        Me.opt_folder.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.opt_folder.Name = "opt_folder"
+        Me.opt_folder.Size = New System.Drawing.Size(58, 19)
+        Me.opt_folder.TabIndex = 1
+        Me.opt_folder.Text = "Folder"
+        Me.opt_folder.UseVisualStyleBackColor = True
         '
-        'ToolStripStatusLabel1
+        'opt_file
         '
-        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
-        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(0, 17)
+        Me.opt_file.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.opt_file.AutoSize = True
+        Me.opt_file.Checked = True
+        Me.opt_file.Location = New System.Drawing.Point(191, 22)
+        Me.opt_file.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.opt_file.Name = "opt_file"
+        Me.opt_file.Size = New System.Drawing.Size(43, 19)
+        Me.opt_file.TabIndex = 0
+        Me.opt_file.TabStop = True
+        Me.opt_file.Text = "File"
+        Me.opt_file.UseVisualStyleBackColor = True
         '
-        'ToolStripStatusLabel2
+        'designnodeaddlink
         '
-        Me.ToolStripStatusLabel2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold)
-        Me.ToolStripStatusLabel2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
-        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
-        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(128, 17)
-        Me.ToolStripStatusLabel2.Text = "ToolStripStatusLabel2"
-        '
-        'SplitContainer1
-        '
-        Me.SplitContainer1.Location = New System.Drawing.Point(135, 58)
-        Me.SplitContainer1.Name = "SplitContainer1"
-        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
-        '
-        'SplitContainer1.Panel1
-        '
-        Me.SplitContainer1.Panel1.Controls.Add(Me.txtworkdescription)
-        '
-        'SplitContainer1.Panel2
-        '
-        Me.SplitContainer1.Panel2.Controls.Add(Me.Panel1)
-        Me.SplitContainer1.Size = New System.Drawing.Size(395, 244)
-        Me.SplitContainer1.SplitterDistance = 130
-        Me.SplitContainer1.TabIndex = 16
-        '
-        'InsertDesignNode
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(577, 425)
-        Me.Controls.Add(Me.SplitContainer1)
-        Me.Controls.Add(Me.StatusStrip1)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.bttnmodifylink)
-        Me.Controls.Add(Me.bttnremovelink)
-        Me.Controls.Add(Me.bttn_addlink)
-        Me.Controls.Add(Me.bttnCANCEL)
-        Me.Controls.Add(Me.bttnOK)
-        Me.Controls.Add(Me.ckfinish)
+        Me.ClientSize = New System.Drawing.Size(609, 138)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.txtworktitle)
+        Me.Controls.Add(Me.txtresourcelink)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtresourcename)
         Me.Controls.Add(Me.Label1)
-        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.Name = "InsertDesignNode"
-        Me.StatusStrip1.ResumeLayout(False)
-        Me.StatusStrip1.PerformLayout()
-        Me.SplitContainer1.Panel1.ResumeLayout(False)
-        Me.SplitContainer1.Panel1.PerformLayout()
-        Me.SplitContainer1.Panel2.ResumeLayout(False)
-        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.SplitContainer1.ResumeLayout(False)
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "designnodeaddlink"
+        Me.Text = "designnodeaddlink"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents txtworktitle As System.Windows.Forms.TextBox
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents txtworkdescription As System.Windows.Forms.TextBox
-    Friend WithEvents ckfinish As System.Windows.Forms.CheckBox
-    Friend WithEvents bttnOK As System.Windows.Forms.Button
-    Friend WithEvents bttnCANCEL As System.Windows.Forms.Button
-    Friend WithEvents Panel1 As System.Windows.Forms.Panel
-    Friend WithEvents bttn_addlink As System.Windows.Forms.Button
-    Friend WithEvents bttnremovelink As System.Windows.Forms.Button
-    Friend WithEvents bttnmodifylink As System.Windows.Forms.Button
-    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents txtresourcename As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Button2 As System.Windows.Forms.Button
-    Friend WithEvents Button3 As System.Windows.Forms.Button
-    Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
-    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
-    Friend WithEvents ToolStripStatusLabel2 As System.Windows.Forms.ToolStripStatusLabel
-    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents txtresourcelink As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents opt_folder As System.Windows.Forms.RadioButton
+    Friend WithEvents opt_file As System.Windows.Forms.RadioButton
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
 End Class
